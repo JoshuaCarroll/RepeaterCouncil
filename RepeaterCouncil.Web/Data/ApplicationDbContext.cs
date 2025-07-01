8c535f6ada70beb53f31e70c1a9481a4e45c3059
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2025-07-01 13:22:34 -0500

    Initial project structure with MVC, EF Core, multi-env config

---
+++ RepeaterCouncil.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RepeaterCouncil.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // example DbSet
+        public DbSet<Repeater> Repeaters { get; set; }
+    }
+}
